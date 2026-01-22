05805f54fc5f98ebd389013b9ac7ed07d9583849
yashikasindhu09-star <yashikasindhu09@gmail.com>
2026-01-22 19:09:55 +0530

    fix axios production baseURL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://future-viz-backend.onrender.com";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
